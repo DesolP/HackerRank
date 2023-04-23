2d6d94fdac507eb10307dd3dfb0c0014ab0fc29e
olatomalczyk <ola.tomalczyk@gmail.com>
2023-04-23 22:34:18 +0200

    Added new unfinished classes - Student and Priorities inside JavaPriorityQueue package. to be finished

---
+++ src/JavaPriorityQueue/Priorities.java
@@ -0,0 +1,24 @@
+package JavaPriorityQueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Priorities {
+    List<Student> studentList = new ArrayList<Student>();
+
+    int id = 0;
+ public List<Student> getStudents(List<String> events){
+
+
+ }
+ public void addStudent(String name, double cgpa){
+     Student student = new Student(id, name, cgpa);
+     studentList.add(student);
+     id++;
+ }
+ public void removeStudent(){
+    Student selectedStudent = studentList.stream().max((a,b)-> (int)a.getCGPA() > (int)b.getCGPA()? (int)a.getCGPA():(int)b.getCGPA()).get();
+     System.out.println(studentList.get(selectedStudent.getId()));
+    studentList.remove(selectedStudent.getId());
+ }
+}
---
+++ src/JavaPriorityQueue/Student.java
@@ -0,0 +1,29 @@
+package JavaPriorityQueue;
+
+public class Student {
+
+
+    int id;
+    String name;
+    double CGPA;
+    //constructor
+    public Student (int id, String name, double CGPA){
+        this.id = id;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getCGPA() {
+        return CGPA;
+    }
+
+
+
+
+
+}
